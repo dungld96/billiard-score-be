4d828e5e4f4c28cb3ef82c511eda3d7e1d000eb1
Dũng Lê <dunglvh96@gmail.com>
2025-11-13 09:11:00 +0000

    fix build

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 // api/index.js
 const serverless = require('serverless-http');
-const { app } = require('../app');
+// `app.js` now default-exports the Express app (module.exports = app)
+// so require the module directly instead of destructuring a named export.
+const app = require('../app');
 
 module.exports = serverless(app);
